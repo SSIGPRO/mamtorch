001e04eb85eafd2470ec2c02b702589500cb8d22
prolucio <luciano.prono@polito.it>
2023-09-04 11:55:12 +0200

    Fixed behaviour

--- mamdense.py
+++ mamdense.py
@@ -1,19 +1,21 @@
 import torch
 import mamtorchkernel
 
-def mam_backward(A, B, Cgrad, argmax, argmin):
+def python_mam_backward(A, B, Cgrad, argmax, argmin):
+    Aaranged_indices = torch.arange(A.shape[0]).reshape(-1, 1)
+    Baranged_indices = torch.tile(torch.arange(B.shape[1]).reshape(1, -1), (1, 2))  
+    arg = torch.concatenate([argmax, argmin], axis=-1).to(int)
+    
     AzeroedT = torch.zeros((A.shape[1], A.shape[0]), device=A.device)
-    aranged_indices = np.arange(A.shape[0]).reshape(-1, 1)
-    AzeroedT[argmax, aranged_indices] = A[aranged_indices, argmax]
-    AzeroedT[argmin, aranged_indices] = A[aranged_indices, argmin]
-    Bgrad = AzeroedT@Cgrad
+    AzeroedT[arg, Aaranged_indices] = A[Aaranged_indices, arg]
+    Bgrad = torch.zeros_like(B)
+    Bgrad[arg, Baranged_indices] = (AzeroedT@Cgrad)[arg, Baranged_indices]
     del AzeroedT
     
     BzeroedT = torch.zeros((B.shape[1], B.shape[0]), device=B.device)
-    aranged_indices = np.arange(B.shape[1]).reshape(1, -1)
-    BzeroedT[aranged_indices, argmax] = B[argmax, aranged_indices]
-    BzeroedT[aranged_indices, argmin] = B[argmin, aranged_indices]
-    Agrad = Cgrad@BzeroedT
+    BzeroedT[Baranged_indices, arg] = B[arg, Baranged_indices]
+    Agrad = torch.zeros_like(A)
+    Agrad[Aaranged_indices, arg] = (Cgrad@BzeroedT)[Aaranged_indices, arg]
     del BzeroedT
     
     return Agrad, Bgrad
