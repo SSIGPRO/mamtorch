bec12ed30be38e9722921a2a8a032c2f456f3b6f
prolucio <luciano.prono@polito.it>
2023-09-21 16:31:38 +0200

    Added max/min selection count function.

--- mamdense.py
+++ mamdense.py
@@ -8,13 +8,13 @@ class MAMDenseFunction(torch.autograd.Function):
     def forward(ctx, A, B):
         C, argmax, argmin = mamtorchkernel.mamdense_forward(A, B)
         ctx.save_for_backward(A, B, argmax, argmin)
-        return C
+        return C, argmax, argmin
 
     @staticmethod
-    def backward(ctx, C_grad):
+    def backward(ctx, C_grad, argmax_grad, argmin_grad):
         A, B, argmax, argmin = ctx.saved_tensors
         A_grad, B_grad = mamtorchkernel.mamdense_backward(A, B, C_grad, argmax, argmin)
-        return A_grad, B_grad
+        return A_grad, B_grad, None, None
     
 
 class MAMDense(torch.nn.Module):
@@ -33,6 +33,11 @@ class MAMDense(torch.nn.Module):
             self.beta = 0.0
         self.beta_decay = beta_decay
         self.beta_epochs = beta_epochs
+        
+        self.max_selection_count = None
+        self.min_selection_count = None
+        self.argmax = None
+        self.argmin = None
         
         self.reset_parameters()
         
@@ -55,8 +60,34 @@ class MAMDense(torch.nn.Module):
             self.beta -= delta_beta
             return
         
+    def reset_selection_count(self):
+        self.max_selection_count = torch.zeros_like(self.weight).to(torch.int32)
+        self.min_selection_count = torch.zeros_like(self.weight).to(torch.int32)
+        
+    def update_selection_count(self):
+        # Use the current values of self.argmax and self.argmin to update the selection count
+        if self.argmax is None or self.argmin is None:
+            raise("No argmax or argmin values have been evaluated yet.")
+            
+        if self.max_selection_count is None or self.min_selection_count is None:
+            self.reset_selection_count()
+            
+        num_rows, num_cols = self.argmax.shape
+        col_indices = torch.arange(num_cols).repeat(num_rows).to(self.argmax.device)
+        coordinates = torch.row_stack((col_indices, self.argmax.flatten()))
+        self.max_selection_count[coordinates[0], coordinates[1]] += 1
+        
+        num_rows, num_cols = self.argmin.shape
+        col_indices = torch.arange(num_cols).repeat(num_rows).to(self.argmin.device)
+        coordinates = torch.row_stack((col_indices, self.argmin.flatten()))
+        self.min_selection_count[coordinates[0], coordinates[1]] += 1
+        
     def forward(self, input):
-        C = MAMDenseFunction.apply(input, self.weight.T.contiguous())
+        C, argmax, argmin = MAMDenseFunction.apply(input, self.weight.T.contiguous())
+        # Save argmax and argmin for external access
+        self.argmax = argmax
+        self.argmin = argmin
+        
         # If self.beta is not 0 MAC output is still computed
         if self.beta >= 10e-5:
             D = F.linear(input, self.weight)
