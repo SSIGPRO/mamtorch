460a9199ff69c2f5369c79d99a554bc50aa8927a
prolucio <luciano.prono@polito.it>
2023-10-11 14:37:06 +0200

    Fixed update_selection_count function

--- mamtorch/mamconv1d.py
+++ mamtorch/mamconv1d.py
@@ -7,19 +7,19 @@ class MAMConv1dFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, X, W, stride):
         Y, argmax, argmin = mamtorchkernel.mamconv1d_forward(X, W, stride)
-        ctx.save_for_backward(X, W, argmax, argmin)
+        ctx.save_for_backward(X, W, argmax, argmin, stride)
         return Y, argmax, argmin
 
     @staticmethod
     def backward(ctx, Y_grad, argmax_grad, argmin_grad):
-        X, W, argmax, argmin = ctx.saved_tensors
-        X_grad, W_grad = mamtorchkernel.mamconv1d_backward(X, W, Y_grad, argmax, argmin)
-        return X_grad, W_grad, None, None
+        X, W, argmax, argmin, stride = ctx.saved_tensors
+        X_grad, W_grad = mamtorchkernel.mamconv1d_backward(X, W, Y_grad, argmax, argmin, stride)
+        return X_grad, W_grad, None, None, None
     
 
 class MAMConv1d(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, bias=True, stride=1, padding=0, padding_mode='zeros', beta=False, beta_decay='linear', beta_epochs=0):
-        super(MAMDense, self).__init__()
+        super(MAMConv1d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
@@ -77,16 +77,16 @@ class MAMConv1d(torch.nn.Module):
         if self.max_selection_count is None or self.min_selection_count is None:
             self.reset_selection_count()
             
-        num_filters, num_channels, num_elements = self.weight.shape
-        filter_indices = torch.arange(num_filters).repeat(num_channels*num_elements).to(self.weight.device)
-        channel_indices = self.argmax.flatten() % self.weight.shape[-2]
-        element_indices = self.argmax.flatten() // self.weight.shape[-2]
+        num_batches, num_channels, num_elements = self.argmax.shape
+        filter_indices = torch.arange(num_channels).repeat_interleaved(num_elements).repeat(num_batches).to(self.weight.device)
+        channel_indices = self.argmax.flatten() % self.weight.shape[1]
+        element_indices = self.argmax.flatten() // self.weight.shape[1]
         self.max_selection_count[filter_indices, channel_indices, element_indices] += 1
         
-        num_filters, num_channels, num_elements = self.weight.shape
-        filter_indices = torch.arange(num_filters).repeat(num_channels*num_elements).to(self.weight.device)
-        channel_indices = self.argmin.flatten() % self.weight.shape[-2]
-        element_indices = self.argmin.flatten() // self.weight.shape[-2]
+        num_batches, num_channels, num_elements = self.argmin.shape
+        filter_indices = torch.arange(num_channels).repeat_interleaved(num_elements).repeat(num_batches).to(self.weight.device)
+        channel_indices = self.argmin.flatten() % self.weight.shape[1]
+        element_indices = self.argmin.flatten() // self.weight.shape[1]
         self.min_selection_count[filter_indices, channel_indices, element_indices] += 1
         
     def forward(self, input):
@@ -120,7 +120,7 @@ class MAMConv1d(torch.nn.Module):
         else:
             raise("Invalid padding value.")
         
-        Y, argmax, argmin = MAMConv1dFunction.apply(input, self.weight.contiguous(), self.stride)
+        Y, argmax, argmin = MAMConv1dFunction.apply(input, self.weight.contiguous(), torch.tensor(self.stride))
         
         # Save argmax and argmin for external use
         self.argmax = argmax
@@ -130,12 +130,12 @@ class MAMConv1d(torch.nn.Module):
         if self.beta >= 10e-5:
             Yb = F.conv1d(input, self.weight, stride=self.stride)
             if self.bias is not None:
-                return (1-self.beta)*Y + self.beta*Yb + self.bias[:, None]
+                return (1-self.beta)*Y + self.beta*Yb + self.bias[None, :, None]
             return (1-self.beta)*Y + self.beta*Yb
         
         # No need to compute MAC output
         if self.bias is not None:
-            return Y + self.bias[:, None]
+            return Y + self.bias[None, :, None]
         
         return Y
         
